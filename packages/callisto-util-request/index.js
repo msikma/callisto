27babcca68eef09ca2888db515714e37cbf3e700
Michiel Sikma <michiel@sikma.org>
2018-04-07 16:26:32 +0200

    Upgrade UA to latest version of Chrome

--- packages/callisto-util-request/index.js
+++ packages/callisto-util-request/index.js
@@ -17,7 +17,7 @@ const cookies = {
 const browserHeaders = {
   'Accept-Language': 'en-US,en;q=0.9,ja;q=0.8,nl;q=0.7,de;q=0.6,es;q=0.5,it;q=0.4,pt;q=0.3',
   'Upgrade-Insecure-Requests': '1',
-  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36',
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
   'Cache-Control': 'max-age=0',
   'Connection': 'keep-alive'
