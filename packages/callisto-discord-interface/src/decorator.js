060f4de65def049202a45c94e8807806bd2786fe
Michiel Sikma <michiel@sikma.org>
2018-04-29 23:03:30 +0200

    Remove old testing code

--- packages/callisto-discord-interface/src/decorator.js
+++ packages/callisto-discord-interface/src/decorator.js
@@ -4,9 +4,7 @@
  */
 
 import readyTask from 'callisto-discord-interface/src/tasks/ready'
-import { taskMandarake } from 'callisto-task-mandarake'
 
 export default (discordClient) => {
   discordClient.on('ready', readyTask)
-  //taskMandarake(discordClient)
 }
