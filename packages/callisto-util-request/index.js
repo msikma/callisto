b2a481c5c3fe691046f90d255c70639977d431c2
Michiel Sikma <michiel@sikma.org>
2018-05-23 21:37:10 +0200

    Use request logic from requestAsBrowser

--- packages/callisto-util-request/index.js
+++ packages/callisto-util-request/index.js
@@ -3,83 +3,16 @@
  * Copyright © 2018, Michiel Sikma
  */
 
-import request from 'request-promise-native'
-import requestSync from 'request'
-import FileCookieStore from 'file-cookie-store'
-
-// Cookie file and jar container.
-const cookies = {
-  file: process.env.COOKIE_FILE,
-  jar: request.jar()
-}
-
-// These headers are sent with each request to make us look more like a real browser.
-const browserHeaders = {
-  'Accept-Language': 'en-US,en;q=0.9,ja;q=0.8,nl;q=0.7,de;q=0.6,es;q=0.5,it;q=0.4,pt;q=0.3',
-  'Upgrade-Insecure-Requests': '1',
-  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
-  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-  'Cache-Control': 'max-age=0',
-  'Connection': 'keep-alive'
-}
-
-/**
- * Sets an array of cookies directly.
- */
-export const setCookies = (cookieStrings, url) => {
-  cookieStrings.forEach(cookieStr => cookies.jar.setCookie(request.cookie(cookieStr), url))
-}
-
-/**
- * Loads cookies from the specified cookies.txt file (or the default file)
- * and loads them into a jar so that we can make requests with them.
- */
-export const loadCookies = (cookieOverride) => {
-  // Cookies exported from the browser in Netscape cookie file format.
-  // These are sent with our request to ensure we have access to logged in pages.
-  const cookieFile = cookieOverride || process.env.COOKIE_FILE
-  const cookieStore = new FileCookieStore(cookieFile, { no_file_error: true })
-  cookies.file = cookieFile
-  cookies.jar = request.jar(cookieStore)
-}
+import requestAsBrowser from 'requestAsBrowser'
+import cookieJar from './cookies'
+export { setCookies, loadCookieFile } from './cookies'
 
 /**
  * Safely requests and returns the HTML for a URL.
  *
  * This mimics a browser request to ensure we don't hit an anti-bot wall.
  */
-export const requestAsBrowser = (url, extraHeaders = {}, gzip = true, noCookies = false, resolveWithFullResponse = false) => (
-  request({
-    url,
-    headers: { ...browserHeaders, ...extraHeaders },
-    jar: noCookies ? null : cookies.jar,
-    gzip,
-    resolveWithFullResponse
-  })
-)
-
-/**
- * Same as requestAsBrowser, but does a POST request and includes form data.
- * This sends a form upload using application/x-www-form-urlencoded.
- */
-export const postAsBrowser = (url, form, extraHeaders = {}, gzip = true, noCookies = false, resolveWithFullResponse = false) => (
-  request.post({
-    url,
-    form,
-    headers: { ...browserHeaders, ...extraHeaders },
-    jar: noCookies ? null : cookies.jar,
-    gzip,
-    resolveWithFullResponse
-  })
-)
-
-/**
- * Alternate version of requestAsBrowser() that takes a callback instead.
- * This is used because request-promise-native currently has a bug related to piping to a file,
- * which we use to download files.
- *
- * See <https://github.com/request/request-promise/issues/90>
- */
-export const requestAsBrowserCb = (url, extraHeaders = {}, cb, gzip = true) => (
-  requestSync({ url, headers: { ...browserHeaders, ...extraHeaders }, jar: cookies.jar, gzip }, cb)
-)
+export const requestURL = async (url, extraHeaders = {}, gzip = true) => {
+  const req = await requestAsBrowser(url, cookieJar.jar, extraHeaders, gzip)
+  return req.body
+}
