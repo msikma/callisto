76d232597c693c60cac3d38a9dccd890db443c2c
Michiel Sikma <michiel@sikma.org>
2018-07-01 15:35:06 +0200

    Remove comments and unused commands

--- packages/callisto-task-mandarake/index.js
+++ packages/callisto-task-mandarake/index.js
@@ -3,10 +3,8 @@
  * Copyright © 2018, Michiel Sikma
  */
 
-import { requestHTML } from 'callisto-util-request'
 import { commandResponder } from 'callisto-discord-interface/src/responder'
 
-import { commandAdd, commandList, commandRemove, commandCategories } from './commands'
 import { actionRunSearches } from './actions'
 
 export const id = 'mandarake'
@@ -14,11 +12,6 @@ const name = 'Mandarake'
 export const color = 0xaf031d
 export const colorAuctions = 0x106770
 const formats = [
-//  ['add', ['keyword'], ['category', 'maxPrice'], 'Adds a new search command', commandAdd],
-//  ['list', [], [], 'Lists all active search queries', commandList],
-//  ['remove', ['id'], [], 'Removes a search command by ID', commandRemove],
-//  ['help', [], [], 'Displays this help message'],
-//  ['categories', [], ['parent'], 'Lists all main categories, or all subcategories under a parent ID', commandCategories]
 ]
 const triggerActions = [
   ['message', commandResponder(id, name, color, formats)]
@@ -30,5 +23,3 @@ const scheduledActions = [
 export const getTaskInfo = () => {
   return { id, name, color, formats, triggerActions, scheduledActions }
 }
-//result = await requestHTML('https://order.mandarake.co.jp/order/listPage/list?upToMinutes=1440')
-//https://order.mandarake.co.jp/order/ListPage/list?upToMinutes=360&lang=ja&categoryCode=00&keyword=pokemon
