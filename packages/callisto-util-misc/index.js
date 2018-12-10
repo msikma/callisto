d38e70d7445531ff7bb1549313b83d699c56c14e
Michiel Sikma <michiel@sikma.org>
2018-12-10 16:20:55 +0100

    Add function to get a Google Maps link from a set of coordinates

--- packages/callisto-util-misc/index.js
+++ packages/callisto-util-misc/index.js
@@ -11,6 +11,7 @@ export { getFormattedDate, getIntegerTimestamp, getExactDuration, getDuration, g
 export { embedTitle, embedDescription, embedDescriptionShort, isHTML, removeEmptyLines, separateMarkdownImages, getImagesFromHTML, limitDescription, htmlToMarkdown, capitalizeFirst, escapeMarkdown } from './text'
 export { registerBotName, showCommandHelp, showCommandUsage, parseCommand, findChannelPath, getChannelFromPath } from './discord'
 export { rssParse, parseFeed, parseFeedURL } from './parse'
+export { mapsCoordsLink } from './url'
 
 // Export slugify directly.
 export { default as slugify } from 'slugify'
