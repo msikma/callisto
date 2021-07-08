1e8b7498a06cd77ae46889846fdb1323ed3b0dac
Michiel Sikma <michiel@sikma.org>
2021-07-08 11:37:13 +0200

    Support using multiple hints

--- packages/callisto-core/util/html.js
+++ packages/callisto-core/util/html.js
@@ -89,8 +89,9 @@ const makeLinksAbsolute = ($, item, objBaseURL) => {
  * Finds a tag with a specific content.
  */
 const findTagContent = ($, tag, contentHint) => {
+  const contentHintList = Array.isArray(contentHint) ? contentHint : [contentHint]
   return $(tag)
-    .filter((_, el) => ~$(el).html().indexOf(contentHint))
+    .filter((_, el) => contentHintList.map(hint => $(el).html().indexOf(hint) > -1).filter(n => n).length > 0)
     .map((_, el) => $(el).html())
     .get()[0]
 }
